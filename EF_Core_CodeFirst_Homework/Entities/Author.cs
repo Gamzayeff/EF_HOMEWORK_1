1a2325a08d7a92eb7e8a22747a020f62a4b2880d
Notebook <Notebook@DESKTOP-QCDM367>
2024-01-27 21:57:06 +0400

    Add project files.

---
+++ EF_Core_CodeFirst_Homework/Entities/Author.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EF_Core_CodeFirst_Homework.Entities;
+public class Author : BaseEntity
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set;}
+    public ICollection<Book> Books { get; set; }
+
+    public Author() 
+    {
+    
+    }
+
+    public Author(string? firstName, string? lastName, ICollection<Book> books)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Books = books;
+    }
+}
